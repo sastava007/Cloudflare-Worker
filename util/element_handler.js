c5a9cf0246f12f2e73a6a23c7c46b59843fb5f91
Shivansh Srivastava <sastava007@gmail.com>
2020-04-15 17:31:54 +0000

    Removed code files

--- util/element_handler.js
+++ util/element_handler.js
@@ -1,50 +1,7 @@
-class changeTitle
-{
-    element(e)
-    {
-        e.setInnerContent("Shivansh's CF Worker");
-    }
-}
+/*
+    Intentionally, I have to delete all the implementations from this file because I have found a huge traffic on this repository which can led to 
+    mass cheating and I don't be a part of this.
 
-class changeTitleHeading
-{
-    element(e)
-    {
-        e.append(": Cloudflare Worker");
-    }
-}
+    PS: The original code is still maintained inside the zip file which is submitted for evaluation.
 
-class changeDescription
-{
-    element(e)
-    {
-        e.setInnerContent("There are 2 variants of this application, and both of them are equally likely to get selected");
-    }
-}
-
-class changeSVGBackground
-{
-    element(e)
-    {
-        e.setAttribute("class", "h-6 w-6 text-red-600");
-    }
-}
-
-class changeHyperlink
-{
-    element(e)
-    {
-        e.setAttribute("href", "https://sastava007.github.io");
-        e.setAttribute("target", "_blank");
-        e.setAttribute("style", "font-weight: bold; background-color: red");
-        e.setInnerContent("See my Portfolio Website");
-    }
-}
-
-module.exports = {
-    changeTitle,
-    changeTitleHeading,
-    changeDescription,
-    changeSVGBackground,
-    changeHyperlink
-}
+*/
